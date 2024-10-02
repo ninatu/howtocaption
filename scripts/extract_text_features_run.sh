4181686bbe4d26b3d885ca7b07a59ab806545d1b
Nina <nina.shvetsova0@gmail.com>
2024-10-02 21:01:48 +0200

    Cleaning

--- scripts/extract_text_features_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in $(seq 0 47)
-do
-  sbatch alignment_scripts/1_extract_text_features.sh $i
-done
--- scripts/extract_video_features_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-for i in $(seq 0 47)
-do
-  sbatch alignment_scripts/1_extract_video_features.sh $i
-done
